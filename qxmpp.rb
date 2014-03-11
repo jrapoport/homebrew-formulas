a9bb517f01e56990d6838ef7ba098b0da5ea3d44
Stephen Birarda <commit@birarda.com>
2014-03-11 12:08:57 -0700

    update qxmpp to handle existing installs of Qt5

--- qxmpp.rb
+++ qxmpp.rb
@@ -1,11 +1,39 @@
 require 'formula'
 
+class Qt5Requirement < Requirement
+  fatal true
+  
+  default_formula 'highfidelity/formulas/qt5'
+  
+  def qt_info
+    `qmake --version`.scan(/Qt version ([\d.]+) in ([\w.\/]+)/)[0]
+  end
+  
+  def message 
+    qt_version, qt_path = qt_info
+    <<-EOS.undent
+    It appears you already have Qt #{qt_version} installed at #{qt_path}.
+    To use Qt 5 that comes with this recipe, first uninstall Qt5, 
+    then run this command again.
+
+    If you would like to keep your installation of Qt #{qt_version} instead of
+    using the one provided with homebrew, install the formula with
+    the `--without-qt5` option.
+    EOS
+  end
+
+  satisfy :build_env => false do
+    qmake_location = which('qmake')
+    qmake_location && qmake_location.include?(HOMEBREW_CeLLAR)
+  end
+end
+
 class Qxmpp < Formula
   homepage 'https://code.google.com/p/qxmpp/'
   url 'https://qxmpp.googlecode.com/files/qxmpp-0.7.6.tar.gz'
   sha1 'a87b4b5c94d1f4dc723cbbb7799cf4067c7e5ea2'
 
-  depends_on 'highfidelity/formulas/qt5'
+  depends_on Qt5Requirement => :recommended
   
   option 'static', 'Compile as static library'
 
