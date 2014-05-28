93f32b5ce1feee513ba0c64b377f3344c5eaf947
Stephen Birarda <commit@birarda.com>
2014-05-28 11:42:29 -0700

    add bottle for patched qt5 on mountain lion

--- qt5.rb
+++ qt5.rb
@@ -27,8 +27,16 @@ class Qt5 < Formula
   homepage 'http://qt-project.org/'
   url 'https://highfidelity-public.s3.amazonaws.com/packages/qt-everywhere-opensource-nocorewlan-src-5.2.0.tar.gz'
   sha1 '07ef1ca133db4a5168d8a716e99d145432832a24'
-  head 'git://gitorious.org/qt/qt5.git', :branch => 'stable',
-    :using => Qt5HeadDownloadStrategy
+  
+  
+  
+  bottle do
+    root_url 'http://highfidelity-public.s3.amazonaws.com/dependencies/qt/'
+    sha1 "960ee7289d253dcba962c9cd17b8db6e516bb7ef" => :mavericks
+    sha1 "147e4f738a15ed55602424344bf7d72440a5f6fe" => :mountain_lion
+  end
+  
+  head 'git://gitorious.org/qt/qt5.git', :branch => 'stable', :using => Qt5HeadDownloadStrategy
     
   keg_only "Qt 5 conflicts Qt 4 (which is currently much more widely used)."
 
