887fe5d20c1a8512e67ed7e219c6f34f37c17816
Stephen Birarda <commit@birarda.com>
2014-12-17 10:06:10 -0800

    Revert "Revert "update to C++11 friendly faceshift formula""
    This reverts commit 3ce717c0b27ee1f688d22950394f84bc51a290e7.

--- faceshift.rb
+++ faceshift.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Faceshift < Formula
   homepage "http://www.faceshift.com/"
-  url "http://highfidelity-public.s3.amazonaws.com/dependencies/faceshift-1.0.tar.gz"
-  sha1 "380c31ef07920230ad312a3ddb6d7f8450d10271"
+  url "http://hifi-public.s3.amazonaws.com/dependencies/faceshift-1.0.tar.gz"
+  sha1 "9bfa572d84de8e7c0d9ad06528a0023557cae06b"
 
   def install
     system "./configure", "--disable-debug",
